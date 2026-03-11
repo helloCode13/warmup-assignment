1683a75f71f34699d91db81ca342c9b03d9d0ff1
MarawanElgendy <marawanelgendy19@gmail.com>
2026-03-11 13:12:03 +0200

    Fixed secondsToLongDuration Function digits pad

--- main.js
+++ main.js
@@ -27,6 +27,7 @@ function secondsToDuration(totalSec) {
   const h = Math.floor(totalSec / 3600);
   const m = Math.floor((totalSec % 3600) / 60);
   const s = totalSec % 60;
+  // Digits fixed to natural number
   return `${h}:${String(m).padStart(2, "0")}:${String(s).padStart(2, "0")}`;
 }
 
